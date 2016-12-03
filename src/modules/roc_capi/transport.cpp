21519ba57a6c240c680b6732c8ccfc5f609e4953
Victor Gaydov <victor@enise.org>
2016-12-03 20:48:52 +0300

    Fix

--- src/modules/roc_capi/transport.cpp
+++ src/modules/roc_capi/transport.cpp
@@ -7,7 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "transport.h"
+#include "roc_capi/transport.h"
 
 #include "roc_core/log.h"
 #include "roc_core/scoped_ptr.h"
